7ede2c6222fce2332fa709e8c685889887f582d0
alechkos <alechqos@gmail.com>
2022-11-25 02:18:55 +0200

    initial-home-page-created

---
+++ Views/Shared/FooterPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TravelAgency.Views.Shared
+{
+    public class FooterPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
